2637d12c10eca5a155025f8ac188139ccd66ef87
Denis Chinin <chinin@immo.ru>
2014-03-04 17:04:23 +0400

    add initial templates

---
+++ API.js
@@ -0,0 +1,110 @@
+exports.api = [
+  {
+    title: 'storage',
+    id: 1
+  },
+  {
+    title: 'save to storage',
+    id: 2
+  },
+  {
+    title: 'get from storage',
+    id: 3
+  },
+  {
+    title: 'clear storage',
+    id: 4
+  },
+  {
+    title: 'remove item from storage',
+    id: 5
+  },
+  {
+    title: 'get DUID',
+    id: 6
+  },
+  {
+    title: 'get MAC',
+    id: 7
+  },
+  {
+    title: 'get IP',
+    id: 8
+  },
+  {
+    title: 'get SDI',
+    id: 9
+  },
+  {
+    title: 'get model code',
+    id: 10
+  },
+  {
+    title: 'get product code',
+    id: 11
+  },
+  {
+    title: 'get platform version',
+    id: 12
+  },
+  {
+    title: 'Volume controls enable',
+    id: 13
+  },
+  {
+    title: 'Volume up/down',
+    id: 14
+  },
+  {
+    title: 'get Volume',
+    id: 15
+  },
+  {
+    title: 'toggleMute',
+    id: 16
+  },
+  {
+    title: 'exit function',
+    id: 17
+  },
+  {
+    title: 'return function',
+    id: 18
+  },
+  {
+    title: 'set aspect ration',
+    id: 19
+  },
+  {
+    title: 'mouse navigation',
+    id: 20
+  },
+  {
+    title: 'gesture navigation',
+    id: 21
+  },
+  {
+    title: 'voice recognition',
+    id: 22
+  },
+  {
+    title: 'screen saver function',
+    id: 23
+  },
+  {
+    title: 'get used memory',
+    id: 24
+  },
+  {
+    title: 'native keyboard',
+    id: 25
+  },
+  {
+    title: 'loading indicator',
+    id: 26
+  },
+  {
+    title: 'Player',
+    id: 27
+  }
+];
---
+++ platforms/lg.js
@@ -0,0 +1,196 @@
+exports.platform = 'LG';
+
+// optional, if exist (ex. samsung 2011, samsung 2012)
+exports.year = '2011';
+
+exports.template = [
+  {
+    title: 'storage',
+    id: 1,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'save to storage',
+    id: 2,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'get from storage',
+    id: 3,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'clear storage',
+    id: 4,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'remove item from storage',
+    id: 5,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'get DUID',
+    id: 6,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'get MAC',
+    id: 7,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'get IP',
+    id: 8,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'get SDI',
+    id: 9,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'get model code',
+    id: 10,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'get product code',
+    id: 11,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'get platform version',
+    id: 12,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'Volume controls enable',
+    id: 13,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'Volume up/down',
+    id: 14,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'get Volume',
+    id: 15,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'toggleMute',
+    id: 16,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'exit function',
+    id: 17,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'return function',
+    id: 18,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'set aspect ration',
+    id: 19,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'mouse navigation',
+    id: 20,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'gesture navigation',
+    id: 21,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'voice recognition',
+    id: 22,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'screen saver function',
+    id: 23,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'get used memory',
+    id: 24,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'native keyboard',
+    id: 25,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'loading indicator',
+    id: 26,
+    enable: '',
+    text: '',
+    link: ''
+  },
+  {
+    title: 'Player',
+    id: 27,
+    enable: '',
+    text: '',
+    link: ''
+  }
+];
